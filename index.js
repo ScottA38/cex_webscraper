e378b54399b8200ce747831c11bbdbd0d803a12b
Scott Anderson <scott.anderson@scandiweb.com>
2022-01-31 13:20:57 +0000

    master
    	- Refactor API functions into separate file

--- index.js
+++ index.js
@@ -2,18 +2,13 @@ var readline = require("readline").createInterface( {
   input: process.stdin,
   output: process.stdout
 })
-var https = require("https");
-var PostcodesIO = require("postcodesio-client");
-var postcodes = new PostcodesIO();
 
-var search_terms = undefined;
+let search_terms = undefined;
+const dataHelper = require('./src/lib/functions.js')
 
 function askForTerms() {
   return new Promise( function (resolve, reject) {
     readline.question("Please enter the title of the dvd (or item) you would like to look for on CEX:\n>",function(terms) {
-      /*readline.question("Do you want to continue with these terms? (Press enter if yes)\n>", function(reply) {
-        if (reply != "\n") { process.exit();}
-      })*/
       resolve(terms);
     })
   })
@@ -37,57 +32,6 @@ function askForLocation() {
   })
 }
 
-function constructPath(data, isProdQry) {
-  if (isProdQry) {
-    console.assert(typeof data == "string", `An incorrect data type of ${typeof data} was provided to construct a 'get products' url path`);
-    return `/v3/boxes?q=${data}&firstRecord=1&count=50&sortBy=relevance&sortOrder=desc`
-  }
-  else {
-    console.assert(typeof data == "object", `An incorrect data type of ${typeof data} was provided to construct a 'get nearest stores' url path`);
-    return `/v3/boxes/${data.boxId}/neareststores?latitude=${data.location[0].toString()}&longitude=${data.location[1].toString()}`
-  }
-}
-
-function getData(params, isProdQry) {
-  return new Promise (function (resolve, reject) {
-      let options = {
-        hostname: "wss2.cex.uk.webuy.io",
-        path: constructPath(params, isProdQry)
-      }
-
-      https.get(options, function(resp) {
-        console.log(resp.statusCode);
-        console.log(`Headers: ${JSON.stringify(resp.headers)}`);
-        resp.setEncoding('utf8');
-        let data = "";
-
-        resp.on('data', function(chunk) {
-          data += chunk;
-        });
-
-        resp.on('end', function() {
-          //console.log(JSON.parse(data).explanation);
-          resolve(data);
-        })
-      }).on("error", function (err) {
-        reject(err);
-    });
-  });
-}
-
-
-function geocoder(postcode) {
-  return new Promise(function(resolve, reject) {
-    postcodes.lookup(postcode).then(function(geocode) {
-      resolve([geocode.latitude, geocode.longitude]);
-    })
-    .catch(function(err) {
-      console.log("There was a problem geocoding the postcode information: " + err.toString());
-    })
-  });
-}
-
-
 askForTerms()
 .then(function(result){
   console.log(`Your search term/s is/are: ${result}`);
@@ -95,14 +39,13 @@ askForTerms()
   //pause, not close should allow the input stream to be resumed later if required
   readline.pause();
 
-  getData(terms, true)
+  dataHelper.getData(terms, true)
   .then(function (result) {
     console.log("resolved!");
     var respJSON = JSON.parse(result);
-    //console.log(respJSON);
     //alias the product data of the response for simpler referencing
     let products = respJSON.response.data.boxes
-    for (var i = 0; i <products.length; i++){
+    for (var i = 0; i < products.length; i++){
       console.log(products[i].boxId);
       console.log(products[i].boxName);
       console.log(products[i].boxName + " sells for: £" + products[i].sellPrice);
---
+++ src/lib/functions.js
@@ -0,0 +1,120 @@
+const https = require("https");
+const http = require("http");
+const PostcodesIO = require("postcodesio-client");
+const postcodes = new PostcodesIO();
+const url = require('url');
+
+/**
+ * @param  {object|string}  param
+ * @param  {string} host
+ * @param  {Boolean} isProdQry
+ *
+ * @return {URL}
+ */
+function constructPath(param, host, isProdQry) {
+  if (isProdQry) {
+    console.assert(typeof param == "string", `An incorrect data type of ${typeof param} was provided to construct a 'get products' url path`);
+    let url = new URL(`/v3/boxes`, host);
+    url.search = new URLSearchParams({
+      q: param,
+      firstRecord: 1,
+      count: 50,
+      sortBy: "relevance",
+      sortOrder: "desc"
+    });
+
+    return url;
+  }
+  else {
+    console.assert(typeof param == "object", `An incorrect data type of ${typeof param} was provided to construct a 'get nearest stores' url path`);
+    let url = new URL(`/v3/boxes/${param.boxId}/neareststores`, host);
+    url.search = new URLSearchParams({
+      latitude: param.location[0].toString(),
+      longitude: param.location[1].toString()
+    });
+
+    return url;
+  }
+}
+
+/**
+ * @param  {object|string} params
+ * @param  {Boolean} isProdQry
+ *
+ * @return {string} JSON-formatted
+ */
+function getData(params, isProdQry) {
+  let url = constructPath(params, "https://wss2.cex.uk.webuy.io", isProdQry);
+
+  return fetch(url);
+}
+
+/**
+ * @param  {URL} options
+ *
+ * @return {string}  JSON-formatted
+ */
+function fetch(url) {
+  let reqModule = url.protocol === 'http:' ? http : https;
+
+  return new Promise (function (resolve, reject) {
+      reqModule.get(url.href, function(resp) {
+        resp.setEncoding('utf8');
+        let data = "";
+
+        resp.on('data', function(chunk) {
+          data += chunk;
+        });
+
+        resp.on('end', function() {
+          resolve(data);
+        })
+      }).on("error", function (err) {
+        reject(err);
+    });
+  });
+
+}
+
+/**
+ * Per UK format
+ * @param  {string} value
+ *
+ * @return {Boolean}
+ */
+function validatePostcode(value) {
+  // inwards code (last 3), plus 1 for 0-index
+  let splitIndex = value.length - 3;
+  let o = value.substring(0, splitIndex);
+  let i = value.substring(splitIndex);
+
+  return
+    value.length <= 7
+    && value.length >= 5
+    && !isNan(o[(o.length - 1)])
+    && !isNan(i[0]);
+}
+
+/**
+ * Find a postcode string via API
+ * @param  {string} postcode
+ *
+ * @return {array}
+ */
+function geocoder(postcode) {
+  return new Promise(function(resolve, reject) {
+    postcodes.lookup(postcode).then(function(geocode) {
+      resolve([geocode.latitude, geocode.longitude]);
+    })
+    .catch(function(err) {
+      console.error("There was a problem geocoding the postcode information: " + err.toString());
+    })
+  });
+}
+
+module.exports = {
+  getData: getData,
+  validatePostcode: validatePostcode,
+  fetch: fetch,
+  geocoder: geocoder
+}
