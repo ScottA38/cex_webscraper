fcd0d788c90680771b6fc3c700b7acaff14175f3
Scott Anderson <scott.anderson@scandiweb.com>
2022-03-18 23:08:18 +0200

    master
    	- Add webpack config transpile ecmascript via babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,60 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+module.exports = [
+  {
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(s(a|c)ss)$/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new NodePolyfillPlugin()
+    ]
   },
-  plugins: [
-		new NodePolyfillPlugin()
-	]
-}
+  {
+    entry: {
+      index: "./index.js",
+      app: "./app.js"
+    },
+    output: {
+      path: path.resolve(__dirname),
+      filename: '[name].babel.js',
+    },
+    target: 'node',
+    externals: [nodeExternals()],
+    externalsPresets: {
+        node: true
+    },
+    module: {
+      rules: [
+        {
+          test: [/\.m?js$/],
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new NodePolyfillPlugin()
+    ]
+  }
+]
